5e59ce655c155e38b87e1f741ac156e927c0e95c
Meghann Stamper <meg.stamp@yahoo.com>
2023-04-04 16:09:47 -0400

    add endpoint and imported tvLibrary

--- src/tvLibrary.js
+++ src/tvLibrary.js
@@ -2,12 +2,11 @@ import { db } from "./dbConnect.js";
 
 export function addNewTv(req, res) {
   const newTv = req.body;
-
   //CREATE: add new tv show
   db.collection("tvShows")
     .add(newTv)
     .then((doc) => res.status(201).send(`New Tv Added: ${doc.id}`))
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err))
 }
 
 export async function getAllTv(req, res) {
